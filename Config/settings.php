511056f9a6678731f9a8da84de95d0855bfa0364
Yeison Tapia <yftapial@ut.edu.co>
2019-07-22 17:41:26 -0500

    + Add setting for keys twillio

--- Config/settings.php
+++ Config/settings.php
@@ -6,4 +6,19 @@ return [
     'view'         => 'textarea',
     'translatable' => false,
   ],
+  'twilio-auth-token' => [
+    'description'  => 'inotification::common.settings.twilio.token',
+    'view'         => 'text',
+    'translatable' => false,
+  ],
+  'twilio-account-sid' => [
+    'description'  => 'inotification::common.settings.twilio.sid',
+  'view'         => 'text',
+    'translatable' => false,
+  ],
+  'twilio-sender' => [
+    'description'  => 'inotification::common.settings.twilio.sender',
+    'view'         => 'text',
+    'translatable' => false,
+  ],
 ];
--- Resources/lang/en/common.php
+++ Resources/lang/en/common.php
@@ -3,6 +3,11 @@
 return [
   'settings' => [
     'whatsapp' => 'Whatsapp Message Default',
+    'twilio' => [
+      'token' => 'TWILIO AUTH TOKEN',
+      'sid' => 'TWILIO ACCOUNT SID',
+      'sender' => 'TWILIO SENDER',
+    ]
   ],
 ];
 
