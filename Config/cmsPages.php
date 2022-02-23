0e3e0c7082fd2d57c982f098efb194a85ece774f
msolano <msolano@imaginacolombia.com>
2022-02-23 17:43:00 -0500

    +Add: config `cmsPages`

---
+++ Config/cmsPages.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+  'admin' => [
+    "setting" => [
+      "permission" => "notification.notifications.manage",
+      "activated" => true,
+      "path" => [
+        "en-us" => "/notifications/setting",
+        "es" => "/notificaciones/configuracion"
+      ],
+      "name" => "notification.admin.providers",
+      "layout" => "qsite/_layouts/master",
+      "page" => "qnotification/_pages/admin/setting/index",
+      "title" => "notification.cms.sidebar.adminConfig.title",
+      "headerTitle" => "notification.cms.sidebar.adminConfig.headerTitle",
+      "icon" => "fab fa-stack-exchange",
+      "authenticated" => true,
+      "subHeader" => [
+        "refresh" => true
+      ]
+    ]
+  ],
+  'panel' => [],
+  'main' => []
+];
