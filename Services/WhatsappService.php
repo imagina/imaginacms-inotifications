e2afd8e48e054eb096da7e519fe8167a51b3d763
Juan Escalante <jcec007@gmail.com>
2023-03-08 17:34:59 -0500

    +Added: whatsappService

---
+++ Services/WhatsappService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Notification\Services;
+
+
+class WhatsappService 
+{
+    
+    /**
+     * @param $data (From Imagina Notification)
+     * @param $template (From another module)
+     */
+    public function createTemplate($provider=null,$data=null,$template=null)
+    {
+
+        // Default parameters (fields)
+        if(is_null($template)){
+            $parameters = [
+                [
+                    "type" => "text",
+                    "text" => $data["message"]
+                ]
+            ];
+        }else{
+            $parameters = $template['parameters'];
+        }
+
+        
+        $template = [
+            'name' => $template['name'] ?? $provider->fields->defaultTemplate,
+            'language' =>  $template['locale'] ?? $provider->fields->defaultTemplateLocale,
+            
+            'components' => [
+                [
+                    "type" => 'body',
+                    "parameters" => $parameters
+                ]
+            ]
+        ];
+
+        return $template;
+    }
+
+
+   
+}
