9908a1c1bbde91e6091fe05623f2a8b48195f350
msolanogithub <msolano2505@gmail.com>
2023-04-27 11:45:27 -0500

    +Update: WhatsappService validate optional template parameters

--- Services/WhatsappService.php
+++ Services/WhatsappService.php
@@ -3,44 +3,30 @@
 namespace Modules\Notification\Services;
 
 
-class WhatsappService 
+class WhatsappService
 {
-    
-    /**
-     * @param $data (From Imagina Notification)
-     * @param $template (From another module)
-     */
-    public function createTemplate($provider=null,$data=null,$template=null)
-    {
 
-        // Default parameters (fields)
-        if(is_null($template)){
-            $parameters = [
-                [
-                    "type" => "text",
-                    "text" => $data["message"]
-                ]
-            ];
-        }else{
-            $parameters = $template['parameters'];
-        }
-
-        
-        $template = [
-            'name' => $template['name'] ?? $provider->fields->defaultTemplate,
-            'language' =>  $template['locale'] ?? $provider->fields->defaultTemplateLocale,
-            
-            'components' => [
-                [
-                    "type" => 'body',
-                    "parameters" => $parameters
-                ]
-            ]
-        ];
-
-        return $template;
+  /**
+   * @param $data (From Imagina Notification)
+   * @param $template (From another module)
+   */
+  public function createTemplate($provider = null, $data = null, $template = null)
+  {
+    // Default parameters (fields)
+    if (is_null($template)) {
+      $parameters = [["type" => "text", "text" => $data["message"]]];
+    } else {
+      $parameters = $template['parameters'];
     }
 
+    $template = [
+      'name' => $template['name'] ?? $provider->fields->defaultTemplate ?? null,
+      'language' => $template['locale'] ?? $provider->fields->defaultTemplateLocale ?? null,
+      'components' => [
+        ["type" => 'body', "parameters" => $parameters]
+      ]
+    ];
 
-   
+    return $template;
+  }
 }
