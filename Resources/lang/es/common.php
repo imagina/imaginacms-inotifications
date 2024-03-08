f4754d2c04c7811d2f1867b287bccf2f132a5043
William Verde <williamverde10@gmail.com>
2024-03-08 14:40:18 -0400

    Added: Trait IsNotificable

--- Resources/lang/es/common.php
+++ Resources/lang/es/common.php
@@ -5,5 +5,9 @@ return [
     "logoEmail" => "Logo Plantilla de Correo",
     'labelTemplateEmail' => 'Layout Notificaciones Correo Electrónico',
     'labelContentEmail' => 'Contenido Notificaciones Correo Electrónico',
+  ],
+  "default" => [
+    'title' => 'Titulo del correo',
+    'message' => 'Mensaje del correo',
   ]
 ];
