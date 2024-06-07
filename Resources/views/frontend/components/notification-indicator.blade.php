a2ce4b2c4543be83e38a7dbb96de1e0d4ae53ff4
William <williamverde10@gmail.com>
2024-06-07 17:06:23 -0400

    Added: blade component notification

---
+++ Resources/views/frontend/components/notification-indicator.blade.php
@@ -0,0 +1,38 @@
+<div id="{{$id}}" class="notification-indicator {{$notiClass}}" @if($unRead) data-toggle="tooltip" data-placement="right" title="Tienes notificaciones sin leer" @endif>
+  <a class="btn-notification" href="{{$route}}" target="_blank">
+    <i class="icon {{$icon}} {{$iconClass}}"></i>
+      @if($unRead)<span class="indicator"></span>@endif
+  </a>
+</div>
+@section('scripts')
+  @parent
+  <style>
+    #{{$id}} .btn-notification {
+      display: inline-flex;
+      flex-direction: column;
+      align-items: stretch;
+      position: relative;
+    }
+    #{{$id}} .icon {
+      color: {{$colorIcon}};
+      font-size: {{$iconFont}};
+    }
+    #{{$id}} .indicator {
+       position: absolute;
+       top: -4px;
+       right: -3px;
+       cursor: inherit;
+       background-color: {{$colorBadge}};
+       color: #fff;
+       height: 8px;
+       width: 8px;
+       border-radius: 4px;
+       vertical-align: baseline;
+    }
+  </style>
+  <script type="text/javascript">
+      $(function () {
+        $('#{{$id}}').tooltip();
+      });
+  </script>
+@endsection
